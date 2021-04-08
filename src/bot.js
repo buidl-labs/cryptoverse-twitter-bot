c9b00acf5e68f5a020c765d7f68d298df73d56f0
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-04-08 10:27:07 +0530

    feat::construction: getNFTfromtokenid - having fetch issues

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,110 @@
 // listen on port so now.sh likes it
 const { createServer } = require("http");
 const https = require("https");
-const axios = require("axios");
+const fetch = require("node-fetch");
+
+const { bytes2Char } = require("@taquito/tzip16");
 const Twit = require("twit"),
   config = require("./config");
-  puppeteer = require("puppeteer"),
 
 // Initialize bot
-const T = new Twit(config.twitterKeys);
+//  let T = new Twit(config.twitterKeys);
 console.log("🎉 twitter bot running 🎉");
 
+const CONTRACT_ADDRESS = "KT1V6cNW5jTUxEwmMhxvNHkMF3Bkm5a9Cfrt";
+const INDEXER_NETWORK = "mainnet";
+
+function sanitizeJsonUri(origin) {
+  if (origin.startsWith("ipfs://")) {
+    return `https://ipfs.io/ipfs/${origin.substring(7)}/`;
+  }
+
+  return null;
+}
+
+function sanitizeIpfsLink(origin) {
+  if (origin.startsWith("ipfs://")) {
+    return origin.substring(7);
+  }
+
+  return null;
+}
+
+async function getNFTfromTokenId(token_id) {
+  const response = await fetch(
+    `https://api.better-call.dev/v1/contract/${INDEXER_NETWORK}/${CONTRACT_ADDRESS}/storage`
+  );
+
+  const result = await response.json();
+  const tokens = result[0].children.find(
+    (elm) => elm.name === "token_metadata"
+  );
+
+  const tokensMetataData = await fetch(
+    `https://api.better-call.dev/v1/bigmap/${INDEXER_NETWORK}/${tokens.value}`
+  );
+  const tokensMetataDataJSON = await tokensMetataData.json();
+  // console.log("tokenMetadata", tokensMetataDataJSON);
+  const num_keys = tokensMetataDataJSON.active_keys;
+  const all_tokens = [];
+  let tk;
+
+  for (let i = 0; i < parseInt(num_keys / 10) + 1; i++) {
+    tk = await fetch(
+      `https://api.better-call.dev/v1/bigmap/${INDEXER_NETWORK}/${
+        tokens.value
+      }/keys?offset=${10 * i}`
+    );
+    all_tokens.push(...(await tk.json()));
+    // console.log("all_tokens", all_tokens.length);
+    if (all_tokens.length == num_keys) break;
+  }
+
+  if (typeof all_tokens === "undefined" || all_tokens.length <= 0) {
+    return [];
+  }
+
+  const grabContent = (elm) => {
+    // console.log("elm", elm);
+    return fetch(
+      sanitizeJsonUri(bytes2Char(elm.data.value.children[1].children[0].value))
+    )
+      .then((res) => res.json())
+      .then((obj) => {
+        // console.log("from indexer", obj);
+        return {
+          tokenId: elm.data.key.value,
+          uri: sanitizeIpfsLink(obj.artifactUri),
+          timestamp: elm.data.timestamp,
+          imageURI: sanitizeIpfsLink(obj.displayUri),
+        };
+      });
+  };
+
+  // console.log("all_tokensx", all_tokens);
+
+  const filtered = await Promise.all(all_tokens.map(grabContent));
+  const token = allTokens.find((bot) => bot.tokenId == token_id);
+  // console.log('filtered allTokens', filtered);
+  return filtered;
+}
+
+const NFT = getNFTfromTokenId(2);
+console.log(NFT);
+
+async function getAllTokensCount() {
+  const response = await fetch(
+    `https://api.better-call.dev/v1/contract/${INDEXER_NETWORK}/${CONTRACT_ADDRESS}/storage`
+  );
+
+  const result = await response.json();
+
+  const getAllTokenObject = result[0].children.find(
+    (elm) => elm.name === "all_tokens"
+  );
+  console.log(getAllTokenObject.children.length);
+  return getAllTokenObject.children.length;
+}
 
 // // Start once
 // tweeter();
