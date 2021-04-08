c933c2781994f6e179bc6e90e6f953bd9e543136
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2021-04-08 19:25:50 +0530

    feat: :sparkles take screenshot of localhost

--- src/bot.js
+++ src/bot.js
@@ -1,21 +1,30 @@
 // listen on port so now.sh likes it
-const { createServer } = require("http");
-const https = require("https");
+const express = require("express");
 const fetch = require("node-fetch");
 const { bytes2Char } = require("@taquito/tzip16");
+const app = express();
+const puppeteer = require("puppeteer");
+
 const Twit = require("twit"),
   config = require("./config");
 // Initialize bot
 //  let T = new Twit(config.twitterKeys);
+
+app.set("view engine", "ejs");
+
 console.log(":tada: twitter bot running :tada:");
 const CONTRACT_ADDRESS = "KT1V6cNW5jTUxEwmMhxvNHkMF3Bkm5a9Cfrt";
 const INDEXER_NETWORK = "mainnet";
+const url = process.env.url;
+const waitForElement = "#cryptobot";
+
 function sanitizeJsonUri(origin) {
   if (origin.startsWith("ipfs://")) {
     return `https://cloudflare-ipfs.com/ipfs/${origin.substring(7)}/`;
   }
   return null;
 }
+
 async function getAllTokens() {
   const response = await fetch(
     `https://api.better-call.dev/v1/contract/${INDEXER_NETWORK}/${CONTRACT_ADDRESS}/storage`
@@ -61,36 +70,52 @@ async function getNFTMetadata(token_id, tokens) {
     imageURI: sanitizeJsonUri(res.displayUri),
   };
 }
-getAllTokens()
-  .then((res) => getNFTMetadata(4, res))
-  .then((token) => console.log(token));
-// // Start once
-// tweeter();
-// // Once every N milliseconds
-// setInterval(tweeter, 60 * 5 * 1000);
-// // Here is the bot!
-// function tweeter() {
-//   // This is a random number bot
-//   var tweet =
-//     "Here's a random number between 0 and 100: " +
-//     Math.floor(Math.random() * 100);
-//   // Post that tweet!
-//   T.post("statuses/update", { status: tweet }, tweeted);
-//   // Callback for when the tweet is sent
-//   function tweeted(err, data, response) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Success: " + data.text);
-//       //console.log(response);
-//     }
-//   }
-// }
-// This will allow the bot to run on now.sh
-const server = createServer((req, res) => {
-  res.writeHead(200);
-  res.send("twitter bot api");
+
+async function run() {
+  console.log("Launching chromium");
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
+  const page = await browser.newPage();
+  console.log("Url", url);
+
+  try {
+    await page.goto(url, { waitUntil: "networkidle0", timeout: 0 });
+    await page.setViewport({ width: 1920, height: 900, deviceScaleFactor: 3 });
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("Waiting for page to load...");
+  const element = await page.$("model-viewer#cryptobot");
+  console.log("got the element");
+  await page.waitForSelector(waitForElement, { timeout: 1000000 });
+  await page.waitForTimeout(5000);
+  await element.screenshot({
+    path: `./images/cryptobot${Date.now()}.jpg`,
+    type: "jpeg",
+  });
+  console.log("CRYPTOBOT SCREENSHOT TAKEN!");
+
+  console.log("CLOSING Chromium!");
+  await page.close();
+  await browser.close();
+}
+
+app.get("/", function(req, res) {
+  getAllTokens()
+    .then((response) => getNFTMetadata(1, response))
+    .then((token) => {
+      console.log(token);
+      res.render("index", { token: token });
+    });
 });
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT);
-console.log(`BOT SERVER RUNNING AT ${PORT}`);
+
+app.listen(PORT, function() {
+  console.log(`BOT SERVER RUNNING AT ${PORT}`);
+  run();
+});
