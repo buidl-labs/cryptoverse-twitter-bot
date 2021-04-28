73ead26e0226d79d4aa5743a01c89ffc33f135f6
Manan Gouhari <gouhari21@hotmail.com>
2021-04-29 01:09:46 +0530

    Wrap in a server to run as a service on render

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const processContractOperations = require("./src/tezosStream");
 const twitterBot = require("./src/twitterBot");
+const runServer = require("./src/botServer");
 
 twitterBot();
 async function main() {
@@ -9,9 +10,13 @@ async function main() {
     1. Listen to contract operations
       1. Fetch image from IPFS
       2. Process the image to add a background
-    3. Post to twitter
+    3. Post to twitter  
   */
-  processContractOperations();
+  const serverRunning = await runServer();
+  if (serverRunning) {
+    console.log("Server running now.");
+    processContractOperations();
+  }
 }
 
 main();
